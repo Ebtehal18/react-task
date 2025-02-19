573dfdc14c85dc9c0cbae7be8a8cba295ccd1b88
Ebtehal18 <ebtehallelsayed18@gmail.com>
2025-02-19 11:08:22 +0200

    modify responsive

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,7 +12,7 @@ export default function About() {
       </div>
      </div>
   </div>
-  <div className="md:absolute right-0 bottom-0 top-0 md:w-[62%] h-full bg-cover md:bg-[url('./colored-rec.png')] bg-[#6D91EE] md:bg-transparent flex justify-center items-center flex-col px-4 md:px-0 py-4 md:py-0">
+  <div className="md:absolute right-0 bottom-0 top-0 md:w-[62%] h-full bg-cover md:bg-[url('/colored-rec.png')] bg-[#6D91EE] md:bg-transparent flex justify-center items-center flex-col px-4 md:px-0 py-4 md:py-0">
   <div className=" md:w-3/4 flex flex-col  text-white  items-end  justify-center ">
   <h2 className="heading-about">Where</h2>
   <p className="max-w-[550px]">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
